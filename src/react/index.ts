049cab85d9d4b211f32b67088f89e6fde136fea4
Wendao <xiewendao123@foxmail.com>
2023-03-06 05:18:23 +0800

    [add] export deepEqual

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,3 +1,7 @@
+import { deepEqual } from 'fast-equals'
+
 export { default as memo } from './memo'
-export {default as handle} from './handle'
-export {default as useDeepMemo} from './useDeepMemo'
+export { default as handle } from './handle'
+export { default as useDeepMemo } from './useDeepMemo'
+
+export { deepEqual }
